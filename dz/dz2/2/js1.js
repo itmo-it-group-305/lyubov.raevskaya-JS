0bfbe0eaab26612799e7c6e9b761d54caa440236
Lyubov Raevskaya <lubraevskaya@gmail.com>
2015-11-30 14:39:46 +0300

    Корявенько сделала домашки

---
+++ dz/dz2/2/js1.js
@@ -0,0 +1,11 @@
+var area = [1, null, 0, null, 1, null, null, null, null];
+
+for (var i=0; i<area.length; i++) {
+    if (i%3) document.write ("<br>"};
+    if (area[i]==1) { document.write ('<td>'+'X'+'</td>'); }
+    else if (area[i]==0) { document.write ('<td>'+'O'+'</td>'); }
+    else document.write (' ');
+            
+    }
+
+    
