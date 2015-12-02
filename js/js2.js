e11a4b192614175f22a77ee8e2415c88a812a477
Lyubov Raevskaya <lubraevskaya@gmail.com>
2015-12-02 15:29:27 +0300

    не доделано

---
+++ js/js2.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Lyubov on 30.11.2015.
+ */
+
+var menu = document.getElementById("menu");
+var child = document.createElement("li");
+//var text = document.createTextNode("input");
+child.setAttribute("class","menu-container");
+
+
+/*child.style.border ="1px solid black";
+child.style.backgroundColor = "white";
+child.style.listStyle = "none";
+child.style.height = "200px";
+child.style.marginRight = "40px";
+child.style.marginTop = "40px";*/
+
+menu.appendChild(child);
+//child.appendChild("input");
