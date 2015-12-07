055684db3e578d536a09a129d3186eb5edf2c011
lampochka60w <lubraevskaya@gmail.com>
2015-12-07 15:36:07 +0300

    Доделала светофор. Работает! ура!

--- js/svet.js
+++ js/svet.js
@@ -1,24 +1,26 @@
 /**
  * Created by Lyubov on 04.12.2015.
  */
-function greenOn(){
+function On(){
 
     var gr = document.getElementById('green');
-    gr.style.backgroundColor='green';
-}
-//var time = Date.now();
-//var intervalGr = setInterval(greenOn(), 1000);
-window.setInterval(greenOn, 1000);
-//if (time == 1000)
-//var intervalID
-//clearInterval(intervalID);
-
-function redOn(){
-
     var rd = document.getElementById('red');
+    if (gr.style.backgroundColor != 'green') { 
+        gr.style.backgroundColor = 'green';
+    rd.style.backgroundColor = 'rgba(225, 0, 0, 0.3)'}
+    else { rd.style.backgroundColor = 'red';
+         gr.style.backgroundColor = 'rgba(0, 128, 0, 0.3)'}
+}
+
+window.setInterval(On, 1000);
+
+
+/*function redOn(){
+
+    
     rd.style.backgroundColor='red';
 }
 
-window.setInterval(redOn, 2000);
+window.setInterval(redOn, 2000);*/
 
-clearInterval(intervalID);
+//clearInterval(intervalID);
